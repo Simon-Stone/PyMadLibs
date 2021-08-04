e7b4020d95582d43180385d6455307f258ef564e
nullpunktTUD <sipreuss@gmail.com>
2021-08-04 12:09:15 +0200

    Implemented function to parse a "Mad Libs"-style string and a function read the string from a file.

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
-
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+from collections import Counter
+from pathlib import Path
+import re
+from typing import Tuple, Union
+from typing import Counter as CounterType
 
 
-# Press the green button in the gutter to run the script.
+def parse_madlibs_string(madlibs: str) -> Tuple[str, CounterType[str]]:
+    """
+    Parses a string in Mad Libs format
+    :param madlibs: A string in Mad Libs format, where some words are replaced by category names in curly braces.
+
+    :return: The original string and a Counter containing categories as keys and their count as value.
+    Example: { 'noun' : 2, 'verb-in-ing-form' : 1 }
+
+    Example:
+    >>> madlibs_string =  'A {noun} was {verb-in-ing-form} to the {noun}.'
+    >>> parse_madlibs_string(madlibs_string)
+    ('A {noun} was {verb-in-ing-form} to the {noun}.', Counter({'noun': 2, 'verb-in-ing-form': 1}))
+    """
+    categories = re.findall(r"{(.*?)}", madlibs, flags=re.MULTILINE)
+    return madlibs, Counter(categories)
+
+
+def read_madlibs_file(path: Union[str, Path]):
+    """ Read a .madlibs_string file and return its parsed content """
+    with open(path, 'r') as mlf:
+        return parse_madlibs_string(mlf.read())
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    import doctest
+    doctest.testmod()
