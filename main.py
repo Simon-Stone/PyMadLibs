3f62da05d36322d1063830f3b9bf1e8b856a9893
nullpunktTUD <sipreuss@gmail.com>
2021-08-04 12:32:24 +0200

    Implemented function to ask user for the required number of words from each category.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from collections import Counter
 from pathlib import Path
 import re
-from typing import Tuple, Union
+from typing import Dict, List, Tuple, Union
 from typing import Counter as CounterType
 
 
@@ -26,6 +26,26 @@ def read_madlibs_file(path: Union[str, Path]):
     """ Read a .madlibs_string file and return its parsed content """
     with open(path, 'r') as mlf:
         return parse_madlibs_string(mlf.read())
+
+
+def get_user_input(categories: CounterType[str]) -> Dict[str, List[str]]:
+    """
+    Prompts the user for the required number of words for each category and returns the lists
+    :param categories: Counter object where each key is a category name.
+    :return: A dictionary where each key is the category name and each value is a list of user-provided words fitting that category.
+    """
+    def sanitize_category_name(name: str):
+        """ Cleans up the category name to make it more human-readable """
+        return name.replace("-", " ").strip()
+
+    words = dict()
+    prompt_string = "Please enter a "
+    for category in categories.elements():
+        if words.get(category):
+            words[category].append(input(prompt_string + sanitize_category_name(category) + ": ").strip().lower())
+        else:
+            words[category] = [input(prompt_string + sanitize_category_name(category) + ": ").strip().lower()]
+    return words
 
 
 if __name__ == '__main__':
